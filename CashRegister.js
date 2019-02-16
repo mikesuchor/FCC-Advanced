f99d9862f4fcfcbc094b5511f04ef813150c92e0
mikesuchor <mikesuchor@gmail.com>
2019-02-15 19:25:00 -0700

    Added conditions for register

--- CashRegister.js
+++ CashRegister.js
@@ -35,14 +35,23 @@ const checkCashRegister = (price, cash, cid) => {
     ["nickel", 0.05],
     ["penny", 0.01]
   ];
-  let cidTotal = 0;
+  
   const output = {
     status: null,
     change: []
   }
-  let change = cash - price;
+  
+  let change = (cash - price).toFixed(2);
 
-  const calculateNumCoins = (change) => {
+  const cidTotal = (cid) => {
+    let cidTotal = 0;
+    for(let i = 0; i < cid.length; i++) {
+      cidTotal += cid[i][1];
+    }
+    return cidTotal.toFixed(2);
+  };
+
+  const calculateChange = (change) => {
     for(let i = 0; i < denominations.length; i++) {
       if (change === 0) {
         
@@ -56,25 +65,22 @@ const checkCashRegister = (price, cash, cid) => {
     }
   }
 
-  const calculatecidTotal = (cid, cidTotal) => {
-    for(let i = 0; i < cid.length; i++) {
-      cidTotal += cid[i][1];
-    }
-    return cidTotal;
-  }
+  console.log(cidTotal(cid));
+  console.log(change);
 
-  cidTotal = calculatecidTotal(cid, cidTotal).toFixed(2);
-  console.log(cidTotal);
-
-  if (cidTotal < change) {
+  if (cidTotal(cid) < change) {
     output.status = "INSUFFICIENT FUNDS";
     output.change = [];
+  } else if (cidTotal(cid) === change) {
+    output.status = "CLOSED";
+    output.change = cid;
   }
 
-  calculateNumCoins(change, cid);
+  calculateChange(change, cid);
 
   return output;
 }
 
 console.log(
-  checkCashRegister(8.11, 9, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+  checkCashRegister(19.50, 20, [["PENNY", 0], ["NICKEL", 0], ["DIME", 0.4], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+  );
