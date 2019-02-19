3e2de604fb8ec70fd44e1e922b20f952a481ca4a
mikesuchor <mikesuchor@gmail.com>
2019-02-18 21:54:39 -0700

    Finished problem

--- CashRegister.js
+++ CashRegister.js
@@ -10,17 +10,6 @@ Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than
 Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
 
 Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
-
-Currency Unit	Amount
-Penny	$0.01 (PENNY)
-Nickel	$0.05 (NICKEL)
-Dime	$0.1 (DIME)
-Quarter	$0.25 (QUARTER)
-Dollar	$1 (DOLLAR)
-Five Dollars	$5 (FIVE)
-Ten Dollars	$10 (TEN)
-Twenty Dollars	$20 (TWENTY)
-One-hundred Dollars	$100 (ONE HUNDRED)
 */
 
 const checkCashRegister = (price, cash, cid) => {
@@ -33,61 +22,68 @@ const checkCashRegister = (price, cash, cid) => {
     "FIVE": 5.00,
     "TEN": 10.00,
     "TWENTY": 20.00,
-    "ONE HUNDRED": 100
+    "ONE HUNDRED": 100.00
   };
   
   const output = {
     status: "",
     change: []
   }
-  
-  const change = parseFloat(cash - price).toFixed(2);
+
+  const changeDue = parseFloat(cash - price).toFixed(2);
   
   // Calculate total change in drawer
   const cidTotal = (cid) => {
     let total = 0;
-    for(let change of cid) {
+    for (let change of cid) {
       total += change[1];
     }
     return total.toFixed(2);
   };
 
-  const calculateChange = (change) => {
+  const calculateChange = (changeDue) => {
     let changeArr = [];
     let numCoins = 0;
-    for(let i = cid.length - 1; i <= 0; i--) {
-      console.log("change", change);
-      console.log("cid", cid[i]);
-      console.log("change / cid", change / cid[i][1]);
-      if (change / denominations[i][1] >= 1) {
-        while (change / denominations[i][1] >= 1) {
+
+    for (let i = cid.length - 1; i >= 0; i--) {
+      const coinName = cid[i][0];
+      const coinValue = denominations[coinName];
+      const cashInDrawer = cid[i][1];
+      let coinAmount = (cashInDrawer / coinValue).toFixed(2);
+      while (changeDue >= coinValue && coinAmount > 0) {
+          changeDue -= coinValue;
+          changeDue = changeDue.toFixed(2);
+          coinAmount--;
           numCoins++;
-          change -= denominations[i][1];
-        }
-        changeArr.push([denominations[i][0], numCoins * denominations[i][1]]);
-        console.log("numCoins * denominations", numCoins * denominations[i][1]);
-        console.log("changeArr", changeArr);
       }
-      numCoins = 0;
+      if (numCoins > 0) {
+        changeArr.push([coinName, numCoins * coinValue]);
+        numCoins = 0;
+      }
     }
+    if (Number(cidTotal(changeArr)) < Number(changeDue)) {
+      output.status = "INSUFFICIENT_FUNDS";
+      return [];
+    }
+    return changeArr;
   }
 
-  console.log("cidTotal", cidTotal(cid));
-  console.log("change", change);
-
-  if (cidTotal < change) {
-    output.status = "INSUFFICIENT FUNDS";
+  if (Number(cidTotal(cid)) < Number(changeDue)) {
+    output.status = "INSUFFICIENT_FUNDS";
     output.change = [];
-  } else if (cidTotal(cid) === change) {
+  } else if (cidTotal(cid) == changeDue) {
     output.status = "CLOSED";
     output.change = cid;
+  } else {
+    output.status = "OPEN";
+    output.change = calculateChange(changeDue);
   }
 
-  calculateChange(change, cid);
+  calculateChange(changeDue, cid);
 
   return output;
 }
 
 console.log(
-  checkCashRegister(19.31, 20, [["PENNY", 0], ["NICKEL", 0], ["DIME", 0.4], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
-  );
+  checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+);
