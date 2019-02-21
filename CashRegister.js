f6aab908b8c86c61e02181182581611d90bd58da
mikesuchor <mikesuchor@gmail.com>
2019-02-20 23:26:17 -0930

    Removed final console.log

--- CashRegister.js
+++ CashRegister.js
@@ -85,6 +85,4 @@ const checkCashRegister = (price, cash, cid) => {
   return output;
 }
 
-console.log(
-  checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
-);
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
